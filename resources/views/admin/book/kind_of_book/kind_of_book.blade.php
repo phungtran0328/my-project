2e3ae9f09dedfae79c0f43218eebec631a252448
PHUNGTRAN <phungb1401175@student.ctu.edu.vn>
2018-10-28 13:16:40 +0700

    push category, detail, kind_of_book !

--- resources/views/admin/book/kind_of_book/kind_of_book.blade.php
+++ resources/views/admin/book/kind_of_book/kind_of_book.blade.php
@@ -13,7 +13,6 @@
         <div class="col-lg-12">
             <h1 class="page-header">Thể loại sách</h1>
         </div>
-        <!-- /.col-lg-12 -->
     </div>
     <div class="row">
         <div class="col-md-12">
