1529a86e2b11ee42dfd61be62f1126dbf52d7ad3
PHUNGTRAN <phungb1401175@student.ctu.edu.vn>
2018-11-12 12:39:30 +0700

    Push !

--- resources/views/admin/manage/user/user.blade.php
+++ resources/views/admin/manage/user/user.blade.php
@@ -12,7 +12,7 @@
             <div class="col-lg-12">
                 <h1 class="page-header">Nhân viên</h1>
             </div>
-            <!-- /.col-lg-12 -->
+
         </div>
         <div class="row">
             <div class="col-md-12">
